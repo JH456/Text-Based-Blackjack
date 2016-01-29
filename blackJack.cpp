9853328b60335c9378deaa92dc8748b60c0277fb
James Harris <jharris99@gatech.edu>
2016-01-28 20:21:19 -0500

    Shortened lines, except long ones with unicode characters, which I'll fix in a few

--- blackJack.cpp
+++ blackJack.cpp
@@ -1,11 +1,12 @@
 /**
 * Author: Jim Harris
 * Title: ASCII BlackJack
-* Description: A BlackJack game with ASCII graphics. Monospace font and single line spacing recommended for optimal appearance.
+* Description: A BlackJack game with ASCII graphics.
+* Monospace font and single line spacing recommended for optimal appearance.
 * Date: 3/4/15
 **/
 
-//Include additional libraries.
+// Include additional libraries.
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
@@ -13,21 +14,22 @@
 #include <time.h>
 #include <math.h>
 
-//Use the standard namespace for convenience sake.
+// Use the standard namespace for convenience sake.
 using namespace std;
 
-//Constant arrays used when drawing the cards.
-const char *vals[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+// Constant arrays used when drawing the cards.
+const char *vals[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J",
+    "Q", "K"};
 const char *suits[] = {"♠", "♥", "♦", "♣"};
 
-//Struct prototypes for the Card and Hand struct.
+// Struct prototypes for the Card and Hand struct.
 struct Card;
 struct Hand;
 
-//Player class allows for some additional organization.
+// Player class allows for some additional organization.
 class Player;
 
-//Function prototypes.
+// Function prototypes.
 Card* dealCard(Hand* deck);
 void printCards(Hand* hand, int n, int segmentNumber);
 void deleteCard(Card* card);
@@ -38,7 +40,8 @@ int sumHand(Hand* hand);
 string getDecision(string messagePrefix);
 void play();
 
-//Card struct is a linked list node. This way, the number of cards a player has can expand or contract.
+// Card struct is a linked list node. This way, the number of cards a player has
+// can expand or contract.
 struct Card {
 	short suit;
 	short val;
@@ -46,7 +49,9 @@ struct Card {
 	Card* next;
 };
 
-//Hand struct is a linked list node. Each hand has the root node for a card linked list. This makes it possible to have multiple hands so a player can split their hand.
+// Hand struct is a linked list node. Each hand has the root node for a card
+// linked list. This makes it possible to have multiple hands so a player can
+// split their hand.
 struct Hand {
 	Card* root;
 	Hand* next;
@@ -57,13 +62,17 @@ public:
 	Player() {
 		hands = new Hand();
 	}
-	//The current hand that the player is using is the last node in the hands linked list.
+
+	// The current hand that the player is using is the last node in the hands
+    // linked list.
 	Hand* getCurrentHand() {
 		Hand* hand = this->hands;
 		while (hand->next != NULL) hand = hand->next;
 		return hand;
 	}
-	//Gets the hand before the current hand (i.e the new one if the current hand is about to be deleted).
+
+	// Gets the hand before the current hand (i.e the new one if the current
+    // hand is about to be deleted).
 	Hand* getNewHand() {
 		Hand* hand = this->hands;
 		if (hand->next == NULL) return hand;
@@ -73,14 +82,25 @@ public:
 	Hand* getRootHand() {
 		return hands;
 	}
-	//Gets input from the player.
-	string getMove(string message = "Enter your move! (hit, split, hold, or fold): ") {
+
+	// Gets input from the player.
+	string getMove(string message =
+        "Enter your move! (hit, split, hold, or fold): ") {
 		cout << message;
 		string move = "";
 		cin >> move;
-		return (move == "hit" || move == "hold" || move == "fold") ? move : (move == "split") ? splittable(this->getCurrentHand()) ? move : getMove("Invalid Move! Cannot split this hand! (hit, hold, or fold) : ") : getMove("Invalid Move! (hit, split, hold, or fold): ");
+		return (move == "hit" || move == "hold" || move == "fold")
+            ? move
+            : (move == "split")
+                ? splittable(this->getCurrentHand())
+                    ? move
+                    : getMove("Invalid Move! Cannot split this hand! "
+                        "(hit, hold, or fold) : ")
+                : getMove("Invalid Move! (hit, split, hold, or fold): ");
 	}
-	//Adds a card to the current hand. Assumes card has already been removed from a deck, and therefore has no child node.
+
+	// Adds a card to the current hand. Assumes card has already been removed
+    // from a deck, and therefore has no child node.
 	void addCard(Card* card) {
 		Hand* hand = getCurrentHand();
 		if (hand->root == NULL) {
@@ -99,16 +119,19 @@ public:
 		}
 		delete card;
 	}
-	//Splits the player's hand by removing the duplicate card from the current hand and adding it to the next hand node in the linked list.
+
+	// Splits the player's hand by removing the duplicate card from the current
+    // hand and adding it to the next hand node in the linked list.
 	void splitCurrentHand() {
 		Hand* hand = this->getCurrentHand();
 		if (!splittable(hand)) return;
 		hand->next = new Hand();
-		//Safe to assume that during a successful split, the card going into the new hand is at the end of a linked list.
+		// Safe to assume that during a successful split, the card going into
+        // the new hand is at the end of a linked list.
 		hand->next->root = hand->root->next;
 		hand->root->next = NULL;
 	}
-	//Deletes linked lists to prevent memory leaks.
+	// Deletes linked lists to prevent memory leaks.
 	~Player() {
 		deleteHand(hands);
 	}
@@ -116,29 +139,30 @@ private:
 	Hand* hands;
 };
 
-//Deletes a linked list of cards.
+// Deletes a linked list of cards.
 void deleteCard(Card* card) {
 	if (card->next != NULL) deleteCard(card->next);
 	delete card;
 }
-//Deletes a linked list of hands.
+// Deletes a linked list of hands.
 void deleteHand(Hand* hand) {
 	if (hand->root != NULL) deleteCard(hand->root);
 	if (hand->next != NULL) deleteHand(hand->next);
 	delete hand;
 }
 
-//Pops a random card from a deck. Just make sure to save this card and delete it later.
+// Pops a random card from a deck. Just make sure to save this card and delete
+// it later.
 Card* dealCard(Hand* deck) {
 	Card* card = deck->root;
 	int c = 0;
-	//Count the number of cards in the deck.
+	// Count the number of cards in the deck.
 	while (card != NULL) {
 		c++;
 		card = card->next;
 	}
 	if (c == 0) return NULL;
-	//Select a random card and pop it from the deck.
+	// Select a random card and pop it from the deck.
 	int n = rand() % c;
 	Card* cardBefore = deck->root;
 	if (n == 0) {
@@ -154,7 +178,8 @@ Card* dealCard(Hand* deck) {
 	return card;
 }
 
-//Prints out up to n cards in a hand. Leave segment number empty in calls. Also prints out some borders for the game table.
+// Prints out up to n cards in a hand. Leave segment number empty in calls.
+// Also prints out some borders for the game table.
 void printCards(Hand* hand, int n = 52, int segmentNumber = 0) {
 	if (segmentNumber >= 5) return;
 
@@ -163,20 +188,24 @@ void printCards(Hand* hand, int n = 52, int segmentNumber = 0) {
 	int i = 0;
 
 	cout << "║ ";
-	
+
 	while (card != NULL && i < n) {
 		switch (segmentNumber) {
 			case 0:
 				cout << "╔═══╗";
 				break;
 			case 1:
-				cout << "║" << suits[card->suit] << (strlen(vals[card->faceVal]) < 2 ? " " : "") << vals[card->faceVal] << "║";
+				cout << "║" << suits[card->suit]
+                    << (strlen(vals[card->faceVal]) < 2 ? " " : "")
+                    << vals[card->faceVal] << "║";
 				break;
 			case 2:
 				cout << "║   ║";
 				break;
 			case 3:
-				cout << "║" << vals[card->faceVal] << (strlen(vals[card->faceVal]) < 2 ? " " : "") << suits[card->suit] << "║";
+				cout << "║" << vals[card->faceVal]
+                    << (strlen(vals[card->faceVal]) < 2 ? " " : "")
+                    << suits[card->suit] << "║";
 				break;
 			default:
 				cout << "╚═══╝";
@@ -190,17 +219,16 @@ void printCards(Hand* hand, int n = 52, int segmentNumber = 0) {
 
 	cout << endl;
 
-	//Recursive call to print out next line.
+	// Recursive call to print out next line.
 	printCards(hand, n, segmentNumber + 1);
 }
 
-//Given an empty hand, populates the hand with a whole french deck of cards.
+// Given an empty hand, populates the hand with a whole french deck of cards.
 void fillDeck(Hand* deck) {
 	if (deck->root != NULL) return;
 	deck->root = new Card();
 	Card* card = deck->root;
-	
-	//Just does one suit for now.
+
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 13; j++) {
 			card->suit = i;
@@ -208,18 +236,22 @@ void fillDeck(Hand* deck) {
 			card->faceVal = j;
 			if (!(i == 3 && j == 12)) {
 				card->next = new Card();
-				card = card->next;	
+				card = card->next;
 			}
 		}
 	}
 }
 
-//Returns whether or not the given hand can be split.
+// Returns whether or not the given hand can be split.
 bool splittable(Hand* hand) {
-	return (hand->root != NULL && hand->root->next != NULL && hand->root->faceVal == hand->root->next->faceVal && hand->root->next->next == NULL);
+	return (hand->root != NULL
+        && hand->root->next != NULL
+        && hand->root->faceVal == hand->root->next->faceVal
+        && hand->root->next->next == NULL);
 }
 
-//Calculates the numerical value of a hand. Takes into account the dual values of aces.
+// Calculates the numerical value of a hand. Takes into account the dual values
+// of aces.
 int sumHand(Hand* hand) {
 	Card* card = hand->root;
 	int sum = 0;
@@ -227,7 +259,7 @@ int sumHand(Hand* hand) {
 		sum += card->val;
 		card = card->next;
 	}
-	//Changes the values of aces from 11 to 1 as neccessary.
+	// Changes the values of aces from 11 to 1 as neccessary.
 	if (sum > 21) {
 		card = hand->root;
 		while (card != NULL && sum > 21) {
@@ -238,21 +270,24 @@ int sumHand(Hand* hand) {
 	return sum;
 }
 
-//Gets the user's decision about whether or not to play another game after the termination of one.
+// Gets the user's decision about whether or not to play another game after the
+// termination of one.
 string getDecision(string messagePrefix = "") {
 	string decision = "";
 	cout << messagePrefix << "Would you like to play again? (y/n): ";
 	cin >> decision;
-	return (decision == "y" || decision == "n") ? decision : getDecision("Invalid decision! ");
+	return (decision == "y" || decision == "n")
+        ? decision
+        : getDecision("Invalid decision! ");
 }
 
-//Plays a game of BlackJack.
+// Plays a game of BlackJack.
 void play() {
-	//Create deck.
+	// Create deck.
 	Hand* deck = new Hand();
 	fillDeck(deck);
 
-	//Create player and dealer, and deal them each two cards.
+	// Create player and dealer, and deal them each two cards.
 	Player player = Player();
 	player.addCard(dealCard(deck));
 	player.addCard(dealCard(deck));
@@ -261,12 +296,12 @@ void play() {
 	dealer.addCard(dealCard(deck));
 	dealer.addCard(dealCard(deck));
 
-	//Whether or not the game is still going on.
+	// Whether or not the game is still going on.
 	bool alive = true;
-	//If the user won; 1 is victory, -1 is defeat by the dealer, 0 is a push.
+	// If the user won; 1 is victory, -1 is defeat by the dealer, 0 is a push.
 	short won = -1;
 	while (alive) {
-		//Clear screen somehow.
+		// Clear screen somehow.
 		printf("\033[2J");
     	printf("\033[%d;%dH", 0, 0);
 
@@ -276,7 +311,7 @@ void play() {
 		int playerSum = sumHand(playerHand);
 		int dealerSum = sumHand(dealerHand);
 
-		//Prints out cards, game table, and numerical values of hands.
+		// Prints out cards, game table, and numerical values of hands.
 		cout << "╔══════════════════════════════════════════════════════════════╦╗" << endl;
 		printCards(dealer.getCurrentHand());
 		cout << "╠══════════════════════════════════════════════════════════════╬╣" << endl;
@@ -287,7 +322,8 @@ void play() {
 		cout << "The dealer's sum is " << dealerSum << endl << endl;
 		cout << "Your sum is " << playerSum << endl << endl;
 
-		//Either switches the player to their next hand or terminates the game if he/she busts.
+		// Either switches the player to their next hand or terminates the game
+        // if he/she busts.
 		if(playerSum > 21) {
 			if (playerHand == player.getRootHand()) alive = false;
 			else {
@@ -297,14 +333,16 @@ void play() {
 				deleteHand(hand);
 			}
 		}
-		//Terminates the game if the dealer wins.
+		// Terminates the game if the dealer wins.
 		else if (dealerSum == 21) alive = false;
-		//Terminates the game with victory if the dealer busts or the player wins.
+		// Terminates the game with victory if the dealer busts or the player
+        // wins.
 		else if (dealerSum > 21 || playerSum == 21) {
 			won = 1;
 			alive = false;
 		}
-		//If the game does not terminate (first case in if-else chain does not always terminate game), queries the user for a decision.
+		// If the game does not terminate (first case in if-else chain does not
+        // always terminate game), queries the user for a decision.
 		if (alive) {
 			string move = player.getMove();
 			if (move == "hit") {
@@ -316,25 +354,32 @@ void play() {
 			else if (move == "fold") {
 				alive = false;
 			}
-			//Update player information.
+			// Update player information.
 			playerHand = player.getCurrentHand();
 			playerSum = sumHand(playerHand);
-			//Give the dealer a card if he wants one.
-			if ((playerSum > dealerSum && playerSum < 21) || dealerSum < 17) dealer.addCard(dealCard(deck));
-			//Terminates the game in a push if the user decides to hold when the dealer refuses to draw more cards.
-			else if (playerSum >= 17 && playerSum == dealerSum && move == "hold") {
+			// Give the dealer a card if he wants one.
+			if ((playerSum > dealerSum && playerSum < 21) || dealerSum < 17)
+                dealer.addCard(dealCard(deck));
+			// Terminates the game in a push if the user decides to hold when
+            // the dealer refuses to draw more cards.
+			else if (playerSum >= 17
+                && playerSum == dealerSum && move == "hold") {
 				won = 0;
 				alive = false;
 			}
 		}
 	}
-	cout << (won == 1 ? "Congratulations! You won!" : (won == 0 ? "Push." : "Sorry, you lost!")) << endl;
+	cout << (won == 1
+        ? "Congratulations! You won!"
+        : (won == 0
+            ? "Push."
+            : "Sorry, you lost!")) << endl;
 	deleteHand(deck);
 }
 
-//Entry point for program
+// Entry point for program
 int main(int argc, char const *argv[]) {
-	
+
 	srand(time(NULL));
 
 	bool playing = true;
